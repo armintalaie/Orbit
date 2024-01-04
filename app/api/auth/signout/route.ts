f2cd41bb8d0c9c97404ced96a1af674e967beae1
Armin Talaie <talaiearmin78@gmail.com>
2024-01-03 18:56:04 -0800

    deply fix

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -2,6 +2,8 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import { type NextRequest, NextResponse } from 'next/server';
 
+const BASE_URL = process.env.BASE_URL;
+
 export async function POST(req: NextRequest) {
   const cookieStore = cookies();
   const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
@@ -13,7 +15,7 @@ export async function POST(req: NextRequest) {
     await supabase.auth.signOut();
   }
 
-  return NextResponse.redirect(req.nextUrl.origin, {
+  return NextResponse.redirect(BASE_URL!, {
     status: 302,
   });
 }
