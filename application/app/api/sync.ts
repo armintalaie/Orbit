a78c5c668dcb5503ff411497a295e34b85b2e38b
Armin Talaie <talaiearmin78@gmail.com>
2024-02-09 23:03:04 -0800

    small change

--- application/app/api/sync.ts
+++ application/app/api/sync.ts
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://localhost:3000');
+const socket = new WebSocket(NEXT_PUBLIC_SYNC_URL || 'ws://localhost:3000'
 
 type WSPubSubMessage = {
     type: "subscribe" | "unsubscribe" | "publish";
